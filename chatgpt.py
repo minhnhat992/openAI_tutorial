8d8bdaf29c9141489188728b92896c434fe67427
minhbui <minh.bui@wirecutter.com>
2023-07-18 14:45:53 -0400

    update

--- chatgpt.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import openai
-
-# key in env
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-chat_completion = openai.ChatCompletion.create(model="")
---
+++ hello_world.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+import openai
+from dotenv import load_dotenv
+
+# key in env
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model_name = "gpt-3.5-turbo"
+
+chat_completion = openai.ChatCompletion.create(
+    model=model_name,
+    messages=[
+        {
+            "role": "user",
+            "content": "Write a 2 paragraphs about gran turismo the video game",
+        }
+    ],
+)
+
+logging.info(chat_completion)
